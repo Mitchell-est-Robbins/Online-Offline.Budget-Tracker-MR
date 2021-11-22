62d6332ca7ead16b84d6dc49382edb4ebe7c0b7d
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-11-21 20:53:44 -0600

    db.js added, connected to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,16 +3,14 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   "/",
+  "db.js",
   "/index.html",
-//   add others
-  
   "/manifest.webmanifest",
   "/style.css",
   "icons/icon-192x192.png",
   "icons/icon-512x512.png",
-
- 
 ];
+//   add others
 
 
 // taken from inclass 12===================================
